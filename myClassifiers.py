830e85ecd55c9c15af1fa063c81111f9e5ef3658
ehsan1222 <s.e.maddahi.ir@gmial.com>
2019-09-04 22:28:27 +0430

    add linear discriminant analysis classifier

--- myClassifiers.py
+++ myClassifiers.py
@@ -1,8 +1,16 @@
 from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
-def naiveBayesClassifier(x_train, y_train, x_test):
+def naive_bayes_classifier(x_train, y_train, x_test):
     gnb = GaussianNB()
     gnb.fit(x_train, y_train)
     y_predict = gnb.predict(x_test)
+    return y_predict
+
+
+def linear_discriminant_analysis_classifier(x_train, y_train, x_test):
+    lda = LinearDiscriminantAnalysis()
+    lda.fit(x_train, y_train)
+    y_predict = lda.predict(x_test)
     return y_predict

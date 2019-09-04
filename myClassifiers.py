33a22f856a082388f7a3f256eea918ce8cae5901
ehsan1222 <s.e.maddahi.ir@gmial.com>
2019-09-04 22:52:08 +0430

    add k nearest neighbor classifier

--- myClassifiers.py
+++ myClassifiers.py
@@ -1,6 +1,8 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+
 
 def naive_bayes_classifier(x_train, y_train, x_test):
     gnb = GaussianNB()
@@ -20,4 +22,11 @@ def support_vector_machine_classifier(x_train, y_train, x_test):
     s_v_m = svm()
     s_v_m.fit(x_train, y_train)
     y_predict = s_v_m.predict(x_test)
+    return y_predict
+
+
+def k_nearest_neighbor_classifier(x_train, y_train, x_test):
+    knn = KNeighborsClassifier()
+    knn.fit(x_train, y_train)
+    y_predict = knn.predict(x_test)
     return y_predict

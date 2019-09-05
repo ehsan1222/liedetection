56edb5f2a96aef7eb5b3cfcf3cded9498b8ce30b
ehsan1222 <s.e.maddahi.ir@gmial.com>
2019-09-05 17:20:49 +0430

    cross validation function

--- MainSrc.py
+++ MainSrc.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 
 def transpose(data):
@@ -32,7 +33,37 @@ def get_data():
     return x_combined, y_combined
 
 
+def cross_validation(y_predict, y_true):
+    correct_count = 0
+    true_pos_count = 0
+    false_pos_count = 0
+    true_neg_count = 0
+    false_neg_count = 0
+    for i in range(len(y_predict)):
+        if y_predict[i] == y_true[i]:
+            correct_count += 1
+            if y_predict[i] == 0:
+                true_pos_count += 1
+            else:
+                true_neg_count += 1
+        else:
+            if y_predict[i] == 0:
+                false_pos_count += 1
+            else:
+                false_neg_count += 1
+    root_mean_squared_error = np.sqrt(mean_squared_error(y_true, y_predict))
+    validate_data = {
+        "correct": correct_count,
+        "true_pos": true_pos_count,
+        "false_pos": false_pos_count,
+        "true_neg": true_neg_count,
+        "false_neg": false_neg_count,
+        "rmse": root_mean_squared_error
+    }
+    return validate_data
+
+
 if __name__ == "__main__":
     x_data, y_data = get_data()
     x_train, y_train, x_test, y_test = train_test_split(x_data, y_data, test_size=.25, random_state=0)
-    
+

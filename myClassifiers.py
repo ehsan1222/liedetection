a5a6bdfd3e6092de7d1e82c04faf1a654b138bee
ehsan1222 <s.e.maddahi.ir@gmial.com>
2019-09-11 11:17:57 +0430

    change neighbor argument in knn to 1

--- myClassifiers.py
+++ myClassifiers.py
@@ -20,21 +20,21 @@ def linear_discriminant_analysis_classifier(x_train, y_train, x_test):
 
 
 def support_vector_machine_classifier(x_train, y_train, x_test):
-    s_v_m = SVC(gamma='auto')
+    s_v_m = SVC()
     s_v_m.fit(x_train, y_train)
     y_predict = s_v_m.predict(x_test)
     return y_predict
 
 
 def k_nearest_neighbor_classifier(x_train, y_train, x_test):
-    knn = KNeighborsClassifier()
+    knn = KNeighborsClassifier(n_neighbors=1)
     knn.fit(x_train, y_train)
     y_predict = knn.predict(x_test)
     return y_predict
 
 
 def multi_layer_perceptron_classifier(x_train, y_train, x_test):
-    mlp = MLPClassifier(hidden_layer_sizes=(5, 3), alpha=1, max_iter=1000)
+    mlp = MLPClassifier(hidden_layer_sizes=(200, 150), activation="logistic", max_iter=1000)
     mlp.fit(x_train, y_train)
     y_predict = mlp.predict(x_test)
     return y_predict
